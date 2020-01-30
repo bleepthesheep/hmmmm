de48ef41cb1667a93f29f46b89c84cb5551d3018
Sheep named Bleep <bleepthesheep@gmail.com>
2020-01-30 12:39:16 +0600

    updates

--- test/main.go
+++ test/main.go
@@ -7,8 +7,5 @@ import (
 )
 
 func main() {
-	fmt.Println(e.Nauuo(50, 43, 20))
-	fmt.Println(e.Nauuo(13, 13, 0))
-	fmt.Println(e.Nauuo(10, 9, 0))
-	fmt.Println(e.Nauuo(5, 9, 2))
+	fmt.Println(e.Priorprime(14))
 }
