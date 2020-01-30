dfada868aa4570e8b8500affa87bdd07a2d94113
Sheep named Bleep <bleepthesheep@gmail.com>
2020-01-30 16:23:15 +0600

    updates

--- test/main.go
+++ test/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	e ".."
 )
 
 func main() {
-	case1 := []int{1, 2, 3, 4}
-	out := e.TwoSum(case1, 5)
-	fmt.Println(out)
+	e.Brainfuck("+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.")
+	// fmt.Println()
 }
