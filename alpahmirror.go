3ab0564dd57e9105cd67689b7d036ac0c535538f
Sheep named Bleep <bleepthesheep@gmail.com>
2020-02-04 12:53:54 +0600

    updates

--- alpahmirror.go
+++ alpahmirror.go
@@ -5,7 +5,13 @@ func AlphaMirror(s string) string {
 
 	result := ""
 	for _, c := range s {
-		out := 'a' + ('z' - c)
+		out := c
+		if c >= 'a' && c <= 'z' {
+			out = 'a' + 'z' - c
+		} else if c >= 'A' && c <= 'Z' {
+			out = 'A' + 'Z' - c
+		}
+
 		result += string(out)
 	}
 
--- test/main.go
+++ test/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	e ".."
 )
 
 func main() {
-	// fmt.Println(e.HiddenP("fgex.;", "tyf34gdgf;'ektufjhgdgex.;.;rtjynur6"))
-	e.PrintMemory([10]int{104, 101, 108, 108, 111, 16, 21, 42})
+	fmt.Println(e.AlphaMirror("abcABC654 12312"))
+	// e.PrintMemory([10]int{104, 101, 108, 108, 111, 16, 21, 42})
 }
